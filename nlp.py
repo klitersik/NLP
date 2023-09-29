7fefa412179ab5f406c9b5368e86bef88ec4582d
klitersik <46646404+klitersik@users.noreply.github.com>
2023-09-29 10:30:13 +0200

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import tempfile
 
 st.title("Audio summarizer")
 st.write("using: whisper-tiny & pegasus_summarizer from Huggingface")
@@ -17,18 +18,20 @@ def query_pegasus(payload,API_URL):
 uploaded_file = st.file_uploader("Upload an audio file", type=["mp3"])
 
 if uploaded_file is not None:
-        bytes_data = uploaded_file.getvalue()
-        #st.write(type(bytes_data))
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+        temp_file.write(uploaded_file.read())
+        audio_file_path = temp_file.name
+
         headers = {"Authorization": "Bearer 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"}
         API_URL_whisper = "https://api-inference.huggingface.co/models/openai/whisper-tiny"
-        output = query_whisper("text",API_URL_whisper)
+        output = query_whisper(audio_file_path,API_URL_whisper)
 
-        text = output
+        text = output["text"]
         st.header("Text")
         st.write(text)
-        st.divider()
+    st.divider()
 
-        API_URL_pegasus = "https://api-inference.huggingface.co/models/tuner007/pegasus_summarizer"
-        output = query_pegasus({"inputs": text,},API_URL_pegasus)
-        st.header("Summarized Text")
-        st.write(output)
+    API_URL_pegasus = "https://api-inference.huggingface.co/models/tuner007/pegasus_summarizer"
+    output = query_pegasus({"inputs": text,},API_URL_pegasus)
+    st.header("Summarized Text")
+    st.write(output)
